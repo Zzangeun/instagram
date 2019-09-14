74ace6c6f3e61d59b410a685f65384d8eb96bea1
Zzangeun <tkddms4199@gmail.com>
2019-09-14 18:05:25 +0900

    first

---
+++ insta/views.py
@@ -0,0 +1,33 @@
+
+from django.shortcuts import render
+from .models import Post
+import operator
+
+def main(request):
+    posts = Post.objects.all()
+    sort = request.GET.get('sort', '')
+
+    if sort == 'new':
+        posts = Post.objects.all()
+    elif sort == 'like_up':
+        ordered_posts ={}
+        post_list = Post.objects.all()
+        for post in post_list:
+            ordered_posts[post] = post.like_count
+        post_list = sorted(ordered_posts.items(), key=operator.itemgetter(1), reverse=True)
+        posts = []
+        for post in post_list:
+            posts.append(post[0])
+    elif sort == 'like_down':
+        ordered_posts ={}
+        post_list = Post.objects.all()
+        for post in post_list:
+            ordered_posts[post] = post.like_count
+        post_list = sorted(ordered_posts.items(), key=operator.itemgetter(1), reverse=False)
+        posts = []
+        for post in post_list:
+            posts.append(post[0])
+
+    return render(request, 'insta/main.html', {
+        'posts':posts,
+    })
