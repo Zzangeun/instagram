433291662d207545066d2b6479c69ecc72b636fa
Zzangeun <48914904+Zzangeun@users.noreply.github.com>
2019-09-23 20:30:55 +0900

    post, comment

--- insta/urls.py
+++ insta/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 
+App_name = 'insta'
+
 urlpatterns = [
     path('', views.main, name="main"),
     path('<int:post_pk>/like/', views.like, name='like'),
+    path('new/', views.new, name='new'),
+    path('<int:post_pk>/edit/', views.edit, name="edit"),
+    path('<int:post_pk>/delete/', views.delete, name="delete"),
+    path('<int:post_pk>/comment/', views.create_comment, name="create_comment"),
+    path('<int:comment_pk>/delete', views.delete_comment, name="delete_comment"),
 ]
--- insta/views.py
+++ insta/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import User, Post, Comment, Like
 import operator
 from django.contrib.auth.decorators import login_required
+from .forms import PostForm, CommentForm
 
 def main(request):
     posts = Post.objects.all()
     sort = request.GET.get('sort', '')
+    comment_form = CommentForm()
 
     if sort == 'new':
         posts = Post.objects.all()
@@ -36,6 +38,7 @@ def main(request):
 
     return render(request, 'insta/main.html', {
         'posts':posts,
+        'comment_form':comment_form,
         'liked_post':liked_post
     })
 
@@ -50,4 +53,57 @@ def like(request, post_pk):
         except:
             Like.objects.create(user=request.user, post=post)
 
+    return redirect('main')
+
+def new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('main')
+    else:
+        form = PostForm()
+    return render(request, 'insta/post_create.html', {
+        'form':form
+    })
+
+def edit(request, post_pk):
+    post = get_object_or_404(Post, pk = post_pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance = post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('main')
+    else:
+        form = PostForm(instance = post)
+    return render(request, 'insta/post_edit.html', {
+        'form':form
+    })
+
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk = post_pk)
+    post.delete()
+    return redirect('main')
+
+def create_comment(request, post_pk):
+    post = get_object_or_404(Post, pk = post_pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+
+            return redirect('main')
+
+def delete_comment(request, comment_pk):
+    comment = get_object_or_404(Comment, pk = comment_pk)
+    post = comment.post
+    comment.delete()
     return redirect('main')
